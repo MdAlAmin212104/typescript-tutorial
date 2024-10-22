139becd82d8eccf075aca530939c675363968828
MD AL AMIN ISLAM <mdalamin212104@gmail.com>
2024-10-22 22:51:25 +0600

    typescript basic text

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 console.log('hello typescript');
 console.log('Now I start typescript learning');
+var userName = 'md al amin islam';
+console.log(userName);
--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
 console.log('hello typescript');
 console.log('Now I start typescript learning');
+const userName: string = 'md al amin islam';
+console.log(userName);
